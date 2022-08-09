dd2bc06cfb857a103d28fa4397c7a2c7f0657be9
Gulzhigit <guljigit.omurkulov@gmail.com>
2022-08-09 14:28:13 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [getValue, setGetValue] = useState("");
+  const [clic, setClick] = useState([]);
+
+  function handle(e) {
+    setGetValue(e.target.value);
+  }
+  function addValue(e) {
+    e.preventDefault();
+    setClick((prev) => {
+      return [...prev, { value: getValue, id: Math.random().toString() }];
+    });
+  }
+  const dalete = (id) => {
+    const newperem = clic.filter((item) => item.id !== id);
+    setClick(newperem);
+  };
+
+  return <div className="App">
+    <div className="cont-1">
+      <input className="input" placeholder="type something..." onChange={(e) => handle(e)} />
+      <button className="btn-1" onClick={addValue}>click</button>
     </div>
-  );
+    {clic.map((item)=>{
+      return(
+        <div className="cont-2">
+          <span className="dateBlock" key={item.id}>
+            {new Date().toDateString()}
+          </span>
+         <h4 className="text">{item.value}</h4>
+         <button className="btn-2" onClick={() => dalete(item.id)}>dalete</button>
+        </div>
+      )
+    })}
+  </div>;
 }
 
 export default App;
